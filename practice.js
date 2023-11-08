dde1f7d6e8083042ee4e55db01ea37740e458ecf
baharkose <baharkse17@gmail.com>
2023-11-08 17:11:30 +0300

    minus plus and trash js codes added

--- practice.js
+++ practice.js
@@ -6,6 +6,14 @@
 
 const bin = document.querySelector(".fa-trash-can")
 const main = document.querySelector("main")
+const minus = document.querySelector(".fa-minus")
+const plus = document.querySelector(".fa-plus")
+const trash = document.querySelector(".fa-trash-can")
+let quantity = document.querySelector(".quantity")
+const products = document.querySelector(".products")
+const product = document.querySelector(".product")
+
+
 console.log(bin);
 
 bin.addEventListener("click", () =>{
@@ -16,5 +24,24 @@ bin.addEventListener("click", () =>{
     
 })
 
+// kural: üstekini bul çağır bubling yap.
 
+products.addEventListener("click", (e) =>{
+  console.log(e.target);
+  if(e.target.classList.contains("fa-plus")){
+    e.target.previousElementSibling.innerText++;
+  }
+
+
+  else if(e.target.classList.contains("fa-minus")){
+    if(e.target.nextElementSibling.innerText > 1){
+      e.target.nextElementSibling.innerText--;
+    }
+    
+  }
+  else if(e.target.classList.contains("fa-trash-can")){
+    e.target.closest(".product").remove()
+  }
+
+})
 
